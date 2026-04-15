cab52e7711db456a67fe251cc932be20f073cc32
bharattech <bharattech@protonmail.com>
2026-04-15 15:12:48 +0530

    next.config.js

--- next.config.js
+++ next.config.js
@@ -7,4 +7,7 @@ const nextConfig = {
   output: "export",
 };
 
+// Disable worker threads for middleware compatibility
+process.env.NEXT_PRIVATE_WORKER_THREADS = 'false';
+
 module.exports = nextConfig;
